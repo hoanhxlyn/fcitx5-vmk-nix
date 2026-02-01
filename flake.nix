77617ea1277fbded36f537b6a64f8e48ae890117
Andrew Nguyen <babybangunny@gmail.com>
2026-02-01 14:25:28 +0700

    add aic8800 usb wifi support

--- flake.nix
+++ flake.nix
@@ -1,60 +1,79 @@
 {
-	description = "Andrewix - flake it till we hit it";
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-		homeManager = {
-			url = "github:nix-community/home-manager";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		nvf = {
-			url = "github:NotAShelf/nvf";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
-	outputs = { self, nixpkgs, homeManager, ... }@inputs:
-		let 
-		system = "x86_64-linux";
-	username = "andrew";
-	hostName = "andrewix";
-	stateVersion = "25.11";
-	fontFamily = "JetBrainsMono Nerd Font";
-	pkgs = nixpkgs.legacyPackages.${system};
-	in {
-		nixosConfigurations.${hostName} = nixpkgs.lib.nixosSystem {
-			inherit system;
-			specialArgs = {
-				inherit inputs username
-				hostName stateVersion
-				fontFamily self;
-			};
-			modules = [ 
-			./nixos/configuration.nix
-			inputs.nvf.nixosModules.default
-			homeManager.nixosModules.home-manager
-			{
-				# nixpkgs.overlays = [
-				# 	inputs.neovimNightly.overlays.default
-				# ];
-				home-manager = {
-					extraSpecialArgs = {
-						inherit inputs username
-						hostName stateVersion
-						fontFamily self;
-					};
-					useGlobalPkgs = true;
-					useUserPackages = true;
-					users.${username} = import ./home.nix;
-					backupFileExtension = "backup";
-				};
-			}
-			];
-		};
-		devShells.${system} = {
-			default = pkgs.mkShell {
-				nativeBuildInputs = with pkgs; [
-					nodejs
-				];
-			};
-		};
-	};
+  description = "Andrewix - flake it till we hit it";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    homeManager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nvf = {
+      url = "github:NotAShelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    aic8800.url = "github:kurumeii/aic8800-nix";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      homeManager,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      username = "andrew";
+      hostName = "andrewix";
+      stateVersion = "25.11";
+      fontFamily = "JetBrainsMono Nerd Font";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations.${hostName} = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {
+          inherit
+            inputs
+            username
+            hostName
+            stateVersion
+            fontFamily
+            self
+            ;
+        };
+        modules = [
+          ./nixos/configuration.nix
+          inputs.nvf.nixosModules.default
+          homeManager.nixosModules.home-manager
+          {
+            # nixpkgs.overlays = [
+            # 	inputs.neovimNightly.overlays.default
+            # ];
+            home-manager = {
+              extraSpecialArgs = {
+                inherit
+                  inputs
+                  username
+                  hostName
+                  stateVersion
+                  fontFamily
+                  self
+                  ;
+              };
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.${username} = import ./home.nix;
+              backupFileExtension = "backup";
+            };
+          }
+          inputs.aic8800.nixosModules.default
+        ];
+      };
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nodejs
+          ];
+        };
+      };
+    };
 }
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,23 +1,33 @@
-{ config, pkgs, hostName, username, stateVersion, ... }:
 {
-	imports = [
-		./hardware-configuration.nix
-		./programs.nix
-		./i18n.nix
-		./users/andrew.nix
-		./fonts.nix
-		./services.nix
-		./neovim.nix
-	];
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
-	boot.supportedFilesystems = ["fuse"];
-	networking.hostName = hostName;
-	networking.networkmanager.enable = true;
-	time.timeZone = "Asia/Ho_Chi_Minh";
-	security.rtkit.enable = true;
-	nix.settings.experimental-features = ["nix-command" "flakes"];
-	nixpkgs.config.allowUnfree = true;
-	system.stateVersion = stateVersion;
-
+  config,
+  pkgs,
+  hostName,
+  username,
+  stateVersion,
+  ...
+}:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./programs.nix
+    ./i18n.nix
+    ./users/andrew.nix
+    ./fonts.nix
+    ./services.nix
+    ./neovim.nix
+  ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "fuse" ];
+  networking.hostName = hostName;
+  networking.networkmanager.enable = true;
+  time.timeZone = "Asia/Ho_Chi_Minh";
+  security.rtkit.enable = true;
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+  nixpkgs.config.allowUnfree = true;
+  hardware.aic8800.enable = true;
+  system.stateVersion = stateVersion;
 }
