e8e130649b6a1715b241ae1fb6d4273bc9d6c047
Andrew Nguyen <babybangunny@gmail.com>
2026-02-09 21:41:46 +0700

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "fcitx5-vmk - Vietnamese Input Method for Fcitx5";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    packages.${system} = {
+      fcitx5-vmk = pkgs.callPackage ./default.nix {};
+    };
+
+    defaultPackage.${system} = self.packages.${system}.fcitx5-vmk;
+  };
+}
